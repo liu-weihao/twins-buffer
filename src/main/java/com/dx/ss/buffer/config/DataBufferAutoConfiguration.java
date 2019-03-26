3efef44586aac76e2326fa5f2ff1ec84da1a68ab
Eric <460181594@qq.com>
2019-03-26 22:06:20 +0800

    中间件改造完毕:bookmark:

--- src/main/java/com/dx/ss/buffer/config/DataBufferAutoConfiguration.java
+++ src/main/java/com/dx/ss/buffer/config/DataBufferAutoConfiguration.java
@@ -2,8 +2,8 @@ package com.dx.ss.buffer.config;
 
 import com.dx.ss.buffer.core.BufferPool;
 import com.dx.ss.buffer.core.DataBuffer;
-import com.dx.ss.buffer.factory.ArrayDataBufferFactory;
 import com.dx.ss.buffer.core.TwinsBufferPool;
+import com.dx.ss.buffer.factory.ArrayDataBufferFactory;
 import com.dx.ss.buffer.factory.DataBufferFactory;
 import com.dx.ss.buffer.storage.MongoPersistStorage;
 import com.dx.ss.buffer.storage.PersistStorage;
@@ -13,8 +13,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableConfigurationProperties(BufferProperties.class)
@@ -72,4 +76,35 @@ public class DataBufferAutoConfiguration {
     public PersistStorage<Object> persistStorage(MongoTemplate mongoTemplate) {
         return new MongoPersistStorage<>(mongoTemplate);
     }
+
+    /**
+     * 注入MongoTemplate
+     *
+     * @param mongoDbFactory MongoDB会话工厂
+     * @return MongoTemplate
+     */
+    @Bean
+    @ConditionalOnMissingBean(MongoTemplate.class)
+    public MongoTemplate getMongoTemplate(MongoDbFactory mongoDbFactory) {
+        return new MongoTemplate(mongoDbFactory);
+    }
+
+    /**
+     * 注入RedisTemplate
+     *
+     * @param redisConnectionFactory redis会话工厂
+     * @return RedisTemplate
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
 }
